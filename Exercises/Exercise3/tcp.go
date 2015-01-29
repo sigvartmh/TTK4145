680d56de9a42ff89ad5dc69a7b1f9641312a2f1d
Sigvart M. Hovland <sigvart.m@gmail.com>
2015-01-29 13:54:25 +0100

    TCP working

--- Exercises/Exercise3/tcp.go
+++ Exercises/Exercise3/tcp.go
@@ -1,80 +1,85 @@
+
 package main
 
-import(
-	"fmt"
-	"net"
-	 "time"
+import (
+        "fmt"
+        "net"
+        "time"
 )
 
 const (
-	server = "129.241.187.xxx"
-	port_fixed = "34933"
-	port_delim = "33546"
+    servAddr = "129.241.187.136"
+    termPort = "33546"
+    host = "129.241.187.104"
 )
 
-func client() {
-    connAddr, err := net.ResolveTCPAddr("tcp","129.241.187.136:33546")
+func TCPclient() {
+    serv, err := net.ResolveTCPAddr("tcp", net.JoinHostPort(servAddr, termPort))
     if err != nil {
-    	Printf("Error in TCP: %s\n", err.Error())
+    	fmt.Println("Error in resolving tcp address:", net.JoinHostPort(servAddr, termPort), err.Error())
     }
-
-    connTCP, err := net.DialTCP("tcp", nil, connAddr)
+    
+    conn, err := net.DialTCP("tcp", nil, serv)
     if err != nil {
-    Printf("Error in TCP: %s\n", err.Error())
+    	fmt.Printf("Error in TCP: %s\n", err.Error())
     }
-
-    message := []byte("Connect to:129.241.187.158:33546\x00")
-    connTCP.Write(message)
-
+    
+    message := []byte("Connect to:"+net.JoinHostPort(host, termPort)+"\x00")
+    conn.Write(message)
+    
 }
 
-func server() {
+func TCPserver() {
 
-	tcpLocalAddr, err := net.ResolveTCPAddr("tcp","129.241.187.158:33546")
+	tcpLocalAddr, err := net.ResolveTCPAddr("tcp","129.241.187.104:33546")
     tcpListener, err := net.ListenTCP("tcp", tcpLocalAddr)
     if err != nil {
-        Println("Error listening:", err.Error())
+        fmt.Println("Error opening connection:", err.Error())
     }
     defer tcpListener.Close()
     for {
         // Listen for an incoming connection.
         conn, err := tcpListener.AcceptTCP()
-        Println("accepted connection")
+        fmt.Println("accepted connection")
         if err != nil {
-            Println("Error accepting: ", err.Error())
+            fmt.Println("Error accepting: ", err.Error())
         }
-
+        
         go handleConnection(conn)
-
+    	
     }
 }
 
 func handleConnection(conn * net.TCPConn){
     data := make([]byte, 1024)
     message := make([]byte, 1024)
-    data = []byte("hello\x00")
+    data = []byte("Cookies for parties\x00")
     for {
-
+    	
     	_, err := conn.Write(data)
-    	Println("msg write\x00")
     	if err != nil {
-    		Printf("Error in TCP: %s\n", err.Error())
+    		fmt.Printf("Error in TCP: %s\n", err.Error())
     		break
     	}
-
+    	
     	time.Sleep(100*time.Millisecond)
+    	
     	conn.Read(message)
-    	Println("msg read")
-    	Printf("Received string: %s\n", message)
+    	fmt.Println("r: ", string(message))
+    	
     }
 }
 
 func main() {
-	doneChan := make(chan string)
+	doneChan := make(chan bool)
 
-	go server()
+	go TCPserver()
 	time.Sleep(100*time.Millisecond)
-	go client()
-
-	Printf(<-doneChan)
+	go TCPclient()
+	
+	<-doneChan
 }
+
+
+
+
