97b71c51f8c5bb87c644bba22430ef9200fcd040
Sigvart M. Hovland <sigvart.m@gmail.com>
2015-01-29 13:28:29 +0100

    Functioning udp listner

---
+++ Exercises/Exercise3/listner2.go
@@ -0,0 +1,68 @@
+package main
+
+import( 
+	"net"
+	"fmt"
+	"time"
+)
+
+const (
+	servAddr = "129.241.187.255"
+	udpPort = "20024"
+	host = "129.241.187.104"
+)
+
+func udpSend(done chan bool){
+	saddr, err := net.ResolveUDPAddr("udp4", net.JoinHostPort(servAddr, udpPort))
+	conn, err := net.DialUDP("udp", nil, saddr)
+
+	if err != nil {
+		fmt.Println("Error connecting to" + servAddr + ":" + udpPort )
+	}
+
+	for {
+		time.Sleep(1000*time.Millisecond)
+		conn.Write([]byte("The cake is a lie"))
+		fmt.Println("Msg sent on udp")
+	}
+	done <- true
+}
+
+
+func udpRecive(done chan bool, port string) {
+	
+	saddr, err := net.ResolveUDPAddr("udp4", net.JoinHostPort(servAddr, udpPort))
+
+	if err != nil{
+		fmt.Println("Failed to resolve address for: " + port)
+
+	}
+
+	buff := make([]byte, 1024)
+
+	l, err := net.ListenUDP("udp4", saddr)
+	if err != nil {
+		fmt.Println("Error listening to" )//+ saddr)
+	}
+
+	for {
+		bytes , remoteAddr, err := l.ReadFromUDP(buff)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+	fmt.Println("Recived ", bytes, " bytes from: ", remoteAddr)
+	fmt.Println(string(buff[:]))
+	} 
+	
+	done<-true
+
+}
+
+func main() {
+	done := make(chan bool)
+	go udpSend(done)
+	go udpRecive(done, udpPort)
+	<-done
+
+}
